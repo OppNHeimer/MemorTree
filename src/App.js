0a5f54190d24adec70890c09ff7701a87043c737
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2017-10-27 11:18:55 -0400

    changed homepage in package json file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Link,
+//   Redirect,
+//   Switch
+// } from "react-router-dom"
 
 import Tree from './components/Tree/Tree.js'
 
 class App extends Component {
   render() {
     return (
+      // <Router>
         <div className="App">
-          <Tree/>
+          {/* <Link to="/memorTree"> */}
+            <Tree/>
+          {/* </Link> */}
         </div>
+      {/* </Router> */}
   
     )
   }
