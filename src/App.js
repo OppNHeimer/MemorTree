ef977c83bbf4f6ff33d546fdd4a620612a3898a9
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2017-10-27 11:31:22 -0400

    page refreshes on click

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import {
-//   BrowserRouter as Router,
-//   Route,
-//   Link,
-//   Redirect,
-//   Switch
-// } from "react-router-dom"
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  Switch
+} from "react-router-dom"
 
 import Tree from './components/Tree/Tree.js'
 
 class App extends Component {
   render() {
     return (
-      // <Router>
+      <Router>
         <div className="App">
-          {/* <Link to="/memorTree"> */}
+          <Link to="/">
             <Tree/>
-          {/* </Link> */}
+          </Link>
         </div>
-      /* </Router> */
+      </Router> 
   
     )
   }
