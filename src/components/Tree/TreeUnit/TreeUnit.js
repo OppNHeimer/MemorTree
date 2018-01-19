c5f417f452f76bff6d4a4c34dff29c68927b7e03
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2018-01-19 11:47:25 -0500

    add readme

--- src/components/Tree/TreeUnit/TreeUnit.js
+++ src/components/Tree/TreeUnit/TreeUnit.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
 export default class TreeUnit extends Component {
-  constructor(x, y, unitType = 'backgroundUnit') {
+  constructor (x, y, unitType = 'backgroundUnit') {
     super()
     this.x = x
     this.y = y
